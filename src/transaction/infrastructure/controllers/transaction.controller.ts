7a9f6cb9464af29911f5d7b3de01c2b79fb75da1
Ayelen Luciano <ayelenluciano@MBP-de-Ayelen.fibertel.com.ar>
2022-12-21 13:07:13 -0300

    fix: resolved return error code

--- src/transaction/infrastructure/controllers/transaction.controller.ts
+++ src/transaction/infrastructure/controllers/transaction.controller.ts
@@ -5,16 +5,31 @@ import { GetLastTransactionFromContractToWalletUseCase } from "../useCases/getLa
 import { GetMostTransactionsAddressFromContractDTO } from "../dto/getMostTransactionsAddressFromContract.dto";
 import { GetMostTransactionsAddressFromContractUseCase } from "../useCases/getMostTransactionsAddressFromContract/getMostTransactionsAddressFromContract.usecase";
 export class TransactionController {
-
-  public async getLastTransactionFromContractToWallet(req: Request, res: Response): Promise<void> {
-    const dto = new GetLastTransactionFromContractToWalletDTO(req);
-    const transaction = await new GetLastTransactionFromContractToWalletUseCase().execute(dto);
-    res.status(200).json(transaction);
+  public async getLastTransactionFromContractToWallet(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const dto = new GetLastTransactionFromContractToWalletDTO(req);
+      const transaction =
+        await new GetLastTransactionFromContractToWalletUseCase().execute(dto);
+      res.status(200).json(transaction);
+    } catch (error) {
+      res.status(error.status).send(error);
+    }
   }
 
-  public async getMostTransactionsWalletFromContract(req: Request, res: Response): Promise<void> {
-    const dto = new GetMostTransactionsAddressFromContractDTO(req);
-    const transaction = await new GetMostTransactionsAddressFromContractUseCase().execute(dto);
-    res.status(200).json(transaction);
+  public async getMostTransactionsWalletFromContract(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const dto = new GetMostTransactionsAddressFromContractDTO(req);
+      const transaction =
+        await new GetMostTransactionsAddressFromContractUseCase().execute(dto);
+      res.status(200).json(transaction);
+    } catch (error) {
+      res.status(error.status).send(error);
+    }
   }
 }
